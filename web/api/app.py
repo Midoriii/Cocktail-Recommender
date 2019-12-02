a5779b32e5d1c3f10a153869cbeaa190e576f2bc
Lukas Matta <lukas@matta.sk>
2019-12-02 20:56:18 +0100

    Add drink endpoint

--- web/api/app.py
+++ web/api/app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
-from recommend import Recommend
+from recommend import Recommend, get_drink
 
 app = Flask(__name__)
 
 @app.route('/recommend', methods=['POST'])
-def index():
+def recommend():
     data = request.json
     recommended_drinks = Recommend(data['indices'], data['count'])
     return jsonify(recommended_drinks)
 
-# @app.route('/ratings', methods=['POST'])
-# def index():
-#     data = request.json
-#     return jsonify(len(data['indices']))
+@app.route('/drink/<int:drink_id>', methods=['GET'])
+def drink(drink_id):
+    drink = get_drink(drink_id, None)
+    return drink
 
 # @app.route('/rate', methods=['POST'])
 # def index():
