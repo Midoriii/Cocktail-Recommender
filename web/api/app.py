a33c9c6bb6cf4778242519bd4afd0aff7096291e
Lukas Matta <lukas@matta.sk>
2019-12-03 19:35:24 +0100

    Improve api

--- web/api/app.py
+++ web/api/app.py
@@ -1,18 +1,22 @@
 from flask import Flask, request, jsonify
 from recommend import Recommend, get_drink
+from flask_cors import CORS
+import simplejson
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
     data = request.json
     recommended_drinks = Recommend(data['indices'], data['count'])
-    return jsonify(recommended_drinks)
+    return simplejson.dumps(recommended_drinks, ignore_nan=True)
+
 
 @app.route('/drink/<int:drink_id>', methods=['GET'])
 def drink(drink_id):
     drink = get_drink(drink_id, None)
-    return drink
+    return simplejson.dumps(drink, ignore_nan=True)
 
 # @app.route('/rate', methods=['POST'])
 # def index():
