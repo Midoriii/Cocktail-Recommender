6dc1f3928fee06afe41f0ebd4d680b86df7e287f
Unknown <stephen.Team24@gmail.com>
2019-12-07 14:32:55 +0100

    Added more graphs concerning algorithms and their intersections

--- algo_eval.py
+++ algo_eval.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def Run():
-  scores = [[0 for x in range(4)] for x in range(2213)] 
+  scores = [[0 for x in range(6)] for x in range(2213)] 
   # Hardcoded because whatever 2213
   for i in range(0,2213):
     lists = recommend.Recommend([i], 10, eval_test = True)
@@ -13,6 +13,8 @@ def Run():
     categories_vs_boosted_categories = list(set(lists[0] + lists[3]))
     combined_vs_boosted_combined = list(set(lists[2] + lists[4]))
     categories_boosted_vs_about_howto = list(set(lists[3] + lists[1]))
+    categories_vs_combined = list(set(lists[0] + lists[2]))
+    categories_boosted_vs_combined_boosted = list(set(lists[3] + lists[4]))
     
     for j in range(0, len(categories_vs_about_howto)):
       if categories_vs_about_howto[j] in lists[0] and categories_vs_about_howto[j] in lists[1]:
@@ -26,31 +28,61 @@ def Run():
     for j in range(0, len(categories_boosted_vs_about_howto)):
       if categories_boosted_vs_about_howto[j] in lists[3] and categories_boosted_vs_about_howto[j] in lists[1]:
         scores[i][3] += 1
+    for j in range(0, len(categories_vs_combined)):
+      if categories_vs_combined[j] in lists[0] and categories_vs_combined[j] in lists[2]:
+        scores[i][4] += 1
+    for j in range(0, len(categories_boosted_vs_combined_boosted)):
+      if categories_boosted_vs_combined_boosted[j] in lists[3] and categories_boosted_vs_combined_boosted[j] in lists[4]:
+        scores[i][5] += 1
   
   scores = np.array(scores)
   #print(scores)
-  plt.hist(scores[:,0])
+  plt.hist(scores[:,0], bins=31, color='blue', rwidth=0.99)
   plt.xlim(0,30)
   plt.title('Categories vs About & How To Make')
-  plt.savefig('graphs/graph1.png')
+  plt.xlabel('Intersection #')
+  plt.ylabel('Count')
+  plt.savefig('graphs/graph1_30.png')
   plt.clf()  
   
-  plt.hist(scores[:,1])
+  plt.hist(scores[:,1], bins=31, color='blue', rwidth=0.99)
   plt.xlim(0,30)
   plt.title('Categories vs Boosted Categories')	
-  plt.savefig('graphs/graph2.png')
+  plt.xlabel('Intersection #')
+  plt.ylabel('Count')
+  plt.savefig('graphs/graph2_30.png')
   plt.clf()  
   
-  plt.hist(scores[:,2])
+  plt.hist(scores[:,2], bins=31, color='blue', rwidth=0.99)
   plt.xlim(0,30)
   plt.title('Combined vs Boosted Combined')
-  plt.savefig('graphs/graph3.png')
+  plt.xlabel('Intersection #')
+  plt.ylabel('Count')
+  plt.savefig('graphs/graph3_30.png')
   plt.clf()  
   
-  plt.hist(scores[:,3])
+  plt.hist(scores[:,3], bins=31, color='blue', rwidth=0.99)
   plt.xlim(0,30)
   plt.title('Boosted Categories vs About & How To Make')	
-  plt.savefig('graphs/graph4.png')
+  plt.xlabel('Intersection #')
+  plt.ylabel('Count')
+  plt.savefig('graphs/graph4_30.png')
+  plt.clf() 
+  
+  plt.hist(scores[:,4], bins=31, color='blue', rwidth=0.99)
+  plt.xlim(0,30)
+  plt.title('Categories vs Combined')	
+  plt.xlabel('Intersection #')
+  plt.ylabel('Count')
+  plt.savefig('graphs/graph5_30.png')
+  plt.clf() 
+  
+  plt.hist(scores[:,5], bins=31, color='blue', rwidth=0.99)
+  plt.xlim(0,30)
+  plt.title('Boosted Categories vs Boosted Combined')	
+  plt.xlabel('Intersection #')
+  plt.ylabel('Count')
+  plt.savefig('graphs/graph6_30.png')
 		
 if __name__ == "__main__":
   Run()
